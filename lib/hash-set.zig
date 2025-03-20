579e5196fa5289f13cabdd8af6cf2f46f20b439d
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2025-03-20 17:46:24 +0000

    Add HashSet.remove()

--- lib/hash-set.zig
+++ lib/hash-set.zig
@@ -77,6 +77,30 @@ pub fn HashSet(comptime T: type) type {
 
             @panic("todo resize");
         }
+
+        pub fn remove(self: Self, value: T) ?T {
+            const hash_value = self.ctx.hash(value);
+            const head_i = hash_value % self.buckets.len;
+            
+            var probe_i: usize = 0;
+
+            while (probe_i < self.buckets.len) : (probe_i += 1) {
+                const i = (head_i + probe_i) % self.buckets.len;
+
+                switch (self.buckets[i]) {
+                    .empty => return null,
+                    .deleted => {},
+                    .full => |full| {
+                        if (full.hash_value == hash_value and self.ctx.equal(full.value, value)) {
+                            defer self.buckets[i] = .deleted;
+                            return full.value;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
     };
 }
 
@@ -103,7 +127,7 @@ test "init() intializes with empty buckets" {
     );
 }
 
-test "add(value) inserts into bucket when value computes to an empty bucket" {
+test "add(value) inserts into bucket when value computes to empty bucket" {
     var set = try testSet(3);
     defer set.deinit();
 
@@ -115,6 +139,51 @@ test "add(value) inserts into bucket when value computes to an empty bucket" {
         &[_]TestSet.Bucket { 
             TestSet.Bucket.empty,
             TestSet.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
+            TestSet.Bucket.empty,
+        },
+        set.buckets
+    );
+}
+
+test "add(value) inserts into bucket when value computes to probed bucket" {
+    var set = try testSet(3);
+    defer set.deinit();
+
+    _ = try set.add(1);
+
+    const ret = try set.add(4);
+
+    try testing.expect(ret);
+    try testing.expectEqualSlices(
+        TestSet.Bucket, 
+        &[_]TestSet.Bucket { 
+            TestSet.Bucket.empty,
+            TestSet.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
+            TestSet.Bucket { .full = .{ .value = 4, .hash_value = 4 } },
+        },
+        set.buckets
+    );
+}
+
+test "add(value) inserts into deleted bucket when value computes to probed bucket across deleted buckets" {
+    var set = try testSet(5);
+    defer set.deinit();
+
+    _ = try set.add(1);
+    _ = try set.add(2);
+    _ = try set.add(3);
+    _ = set.remove(2);
+
+    const ret = try set.add(7);
+
+    try testing.expect(ret);
+    try testing.expectEqualSlices(
+        TestSet.Bucket, 
+        &[_]TestSet.Bucket { 
+            TestSet.Bucket.empty,
+            TestSet.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
+            TestSet.Bucket { .full = .{ .value = 7, .hash_value = 7 } },
+            TestSet.Bucket { .full = .{ .value = 3, .hash_value = 3 } },
             TestSet.Bucket.empty,
         },
         set.buckets
@@ -162,28 +231,98 @@ test "add(value) does not insert when value is present at probed bucket" {
     );
 }
 
-// TODO: test for not inserting when value present beyond tombstones
-// TODO: test for inserting at tombstone when value not present
+test "add(value) does not insert when value is present at probed bucket across deleted buckets" {
+    var set = try testSet(5);
+    defer set.deinit();
 
-test "add(value) inserts at next empty bucket when head bucket collides" {
+    _ = try set.add(6);
+    _ = try set.add(1);
+    _ = try set.add(3);
+    _ = set.remove(1);
+
+    const ret = try set.add(3);
+
+    try testing.expect(!ret);
+    try testing.expectEqualSlices(
+        TestSet.Bucket, 
+        &[_]TestSet.Bucket { 
+            TestSet.Bucket.empty,
+            TestSet.Bucket { .full = .{ .value = 6, .hash_value = 6 } },
+            TestSet.Bucket.deleted,
+            TestSet.Bucket { .full = .{ .value = 3, .hash_value = 3 } },
+            TestSet.Bucket.empty,
+        },
+        set.buckets
+    );
+}
+
+test "remove(value) marks the bucket as deleted when value present at head bucket" {
+    var set = try testSet(5);
+    defer set.deinit();
+
+    _ = try set.add(1);
+    const ret = set.remove(1);
+
+    try testing.expectEqual(1, ret);
+    try testing.expectEqualSlices(
+        TestSet.Bucket,
+        &[_]TestSet.Bucket { 
+            TestSet.Bucket.empty,
+            TestSet.Bucket.deleted,
+            TestSet.Bucket.empty,
+            TestSet.Bucket.empty,
+            TestSet.Bucket.empty,
+        },
+        set.buckets
+    );
+}
+
+test "remove(value) marks the bucket as deleted when value present at probed bucket" {
+    var set = try testSet(5);
+    defer set.deinit();
+
+    _ = try set.add(11);
+    _ = try set.add(6);
+    _ = try set.add(1);
+
+    const ret = set.remove(1);
+
+    try testing.expectEqual(1, ret);
+    try testing.expectEqualSlices(
+        TestSet.Bucket,
+        &[_]TestSet.Bucket { 
+            TestSet.Bucket.empty,
+            TestSet.Bucket { .full = .{ .value = 11, .hash_value = 11 } },
+            TestSet.Bucket { .full = .{ .value = 6, .hash_value = 6 } },
+            TestSet.Bucket.deleted,
+            TestSet.Bucket.empty,
+        },
+        set.buckets
+    );
+}
+
+test "remove(value) returns null when value not present" {
     var set = try testSet(5);
     defer set.deinit();
 
     _ = try set.add(1);
     _ = try set.add(2);
-    const ret = try set.add(11);
+    _ = try set.add(3);
 
-    try testing.expect(ret);
+    const ret = set.remove(4);
+
+    try testing.expectEqual(null, ret);
     try testing.expectEqualSlices(
-        TestSet.Bucket, 
+        TestSet.Bucket,
         &[_]TestSet.Bucket { 
             TestSet.Bucket.empty,
             TestSet.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
             TestSet.Bucket { .full = .{ .value = 2, .hash_value = 2 } },
-            TestSet.Bucket { .full = .{ .value = 11, .hash_value = 11 } },
+            TestSet.Bucket { .full = .{ .value = 3, .hash_value = 3 } },
             TestSet.Bucket.empty,
         },
         set.buckets
     );
+
 }
 
