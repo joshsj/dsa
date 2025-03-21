9969f0912d8887550c7e5f36e9f7381600f35a18
Joshua Sexton-Jones <33764106+joshsj@users.noreply.github.com>
2025-03-21 16:51:46 +0000

    Refactor HashMap to hold key & value

--- lib/hash-map.iterator.zig
+++ lib/hash-map.iterator.zig
@@ -4,23 +4,27 @@ const testing = @import("testing.zig");
 const common = @import("common.zig");
 const HashMap = @import("hash-map.zig").HashMap;
 
-pub fn HashMapIterator(comptime T: type) type {
+pub fn HashMapIterator(comptime TKey: type, comptime TValue: type) type {
     return struct {
         const Self = @This();
+        const Pair = HashMap(TKey, TValue).Pair;
         
-        buckets: []const HashMap(T).Bucket,
+        buckets: []const HashMap(TKey, TValue).Bucket,
         moved: bool = false,
 
-        pub fn init(map: HashMap(T)) Self {
+        pub fn init(map: HashMap(TKey, TValue)) Self {
             return Self { .buckets = map.buckets };
         }
 
-        pub fn curr(self: Self) ?T {
-            return if (self.moved and self.buckets.len > 0) self.buckets[0].full.value
-                   else null;
+        pub fn curr(self: Self) ?Pair {
+            const bucket =
+                if (self.moved and self.buckets.len > 0) self.buckets[0].full
+                else return null;
+
+            return .{ .key = bucket.key, .value = bucket.value, };
         }
         
-        pub fn next(self: *Self) !?T {
+        pub fn next(self: *Self) !?Pair {
             if (self.moved) {
                 self.buckets =
                     if (self.buckets.len > 0) self.buckets[1..] 
@@ -39,9 +43,11 @@ pub fn HashMapIterator(comptime T: type) type {
 }
 
 test HashMapIterator {
-    var map = try HashMap(usize).initCapacity(
+    const Str = [:0]const u8;
+
+    var map = try HashMap(usize, Str).initCapacity(
         testing.allocator,
-        HashMap(usize).Context {
+        .{
             .equal = common.defaultEqual(usize),
             .hash = common.identity(usize),
         },
@@ -49,19 +55,23 @@ test HashMapIterator {
     );
     defer map.deinit();
 
-    _ = try map.add(0);
+    _ = try map.add(0, "0");
     // 1 empty
-    _ = try map.add(2);
-    _ = try map.add(3);
-    _ = try map.add(4);
+    _ = try map.add(2, "2");
+    _ = try map.add(3, "3");
+    _ = try map.add(4, "4");
 
     _ = map.remove(3);
 
-    const expected = &[_]usize { 0, 2, 4 };
+    const expected = &[_]HashMap(usize, Str).Pair {
+        .{ .key = 0, .value = "0" },
+        .{ .key = 2, .value = "2" },
+        .{ .key = 4, .value = "4" },
+    };
 
     try testing.expectEqualSliceToIter(
-        usize,
+        HashMap(usize, Str).Pair,
         expected,
-        HashMapIterator(usize).init(map),
+        HashMapIterator(usize, Str).init(map),
     );
 }
--- lib/hash-map.zig
+++ lib/hash-map.zig
@@ -6,17 +6,18 @@ const common = @import("common.zig");
 const Equal = common.Equal;
 const Hash = common.Hash;
 
-pub fn HashMap(comptime T: type) type {
+pub fn HashMap(comptime TKey: type, comptime TValue: type) type {
     return struct {
         const DefaultCapacity = 4; // Picked at random
         const LoadFactor: f64 = 0.75; // Stolen from the internet
 
         const Self = @This();
-        const Iterator = @import("hash-map.iterator.zig").HashMapIterator(T);
+        const Iterator = @import("hash-map.iterator.zig").HashMapIterator(TKey, TValue);
 
-        pub const Context = struct { hash: *const Hash(T), equal: *const Equal(T) };
+        pub const Context = struct { hash: *const Hash(TKey), equal: *const Equal(TKey) };
 
-        pub const FullBucket = struct { value: T, hash_value: usize, };
+        pub const Pair = struct { key: TKey, value: TValue };
+        pub const FullBucket = struct { key: TKey, value: TValue, hash_value: usize, };
         pub const Bucket = union(enum) {
             empty,
             deleted,
@@ -50,12 +51,12 @@ pub fn HashMap(comptime T: type) type {
         /// Returns true if the value was added
         ///
         /// O(1)
-        pub fn add(self: *Self, value: T) Allocator.Error!bool {
+        pub fn add(self: *Self, key: TKey, value: TValue) Allocator.Error!bool {
             if (self.loadFactor(1) > LoadFactor) {
                 try self.rehash();
             }
 
-            const hash_value = self.ctx.hash(value);
+            const hash_value = self.ctx.hash(key);
             const head_i = hash_value % self.buckets.len;
             var probe_i: usize = 0;
 
@@ -68,14 +69,14 @@ pub fn HashMap(comptime T: type) type {
                 switch (bucket_p.*) {
                     .empty => {
                         (first_deleted_p orelse bucket_p).* = Bucket {
-                            .full = .{ .value = value, .hash_value = hash_value }
+                            .full = .{ .key = key, .value = value, .hash_value = hash_value }
                         };
 
                         self.len += 1;
                         return true;
                     },
 
-                    .full => |full| if (full.hash_value == hash_value and self.ctx.equal(full.value, value)) return false,
+                    .full => |full| if (full.hash_value == hash_value and self.ctx.equal(full.key, key)) return false,
 
                     .deleted => if (first_deleted_p == null) { first_deleted_p = bucket_p; }
                 }
@@ -84,19 +85,19 @@ pub fn HashMap(comptime T: type) type {
             unreachable;
         }
 
-        pub fn remove(self: *Self, value: T) ?T {
+        pub fn remove(self: *Self, key: TKey) ?TValue {
             if (self.len == 0) {
                 return null;
             }
 
-            const bucket_p = self.bucketContaining(value) orelse return null;
+            const bucket_p = self.bucketContaining(key) orelse return null;
 
             defer bucket_p.* = .deleted;
             defer self.len -= 1;
             return bucket_p.full.value;
         }
 
-        pub fn has(self: Self, value: T) bool {
+        pub fn has(self: Self, value: TKey) bool {
             return self.bucketContaining(value) != null;
         }
 
@@ -127,16 +128,8 @@ pub fn HashMap(comptime T: type) type {
             self.buckets = buckets;
         }
 
-        fn loadFactor(self: Self, plus: usize) f64 {
-            // TODO: not safe!
-            const len: f64 = @floatFromInt(self.len + plus);
-            const cap: f64 = @floatFromInt(self.buckets.len);
-
-            return len / cap;
-        }
-
-        fn bucketContaining(self: Self, value: T) ?*Bucket {
-            const hash_value = self.ctx.hash(value);
+        fn bucketContaining(self: Self, key: TKey) ?*Bucket {
+            const hash_value = self.ctx.hash(key);
             const head_i = hash_value % self.buckets.len;
             
             var probe_i: usize = 0;
@@ -148,7 +141,7 @@ pub fn HashMap(comptime T: type) type {
                     .empty => return null,
                     .deleted => {},
                     .full => |full| {
-                        if (full.hash_value == hash_value and self.ctx.equal(full.value, value)) {
+                        if (full.hash_value == hash_value and self.ctx.equal(full.key, key)) {
                             return bucket_p;
                         }
                     }
@@ -156,6 +149,14 @@ pub fn HashMap(comptime T: type) type {
             }
 
             return null;
+        }
+
+        fn loadFactor(self: Self, plus: usize) f64 {
+            // TODO: not safe!
+            const len: f64 = @floatFromInt(self.len + plus);
+            const cap: f64 = @floatFromInt(self.buckets.len);
+
+            return len / cap;
         }
 
         fn alloc(allocator: Allocator, len: usize) Allocator.Error![]Bucket {
@@ -166,7 +167,8 @@ pub fn HashMap(comptime T: type) type {
     };
 }
 
-const TestMap = HashMap(usize);
+const Str = [:0]const u8;
+const TestMap = HashMap(usize, Str);
 
 fn testMap(capacity: usize) Allocator.Error!TestMap {
     return try TestMap.initCapacity(
@@ -195,14 +197,14 @@ test "add(value) inserts into bucket when value computes to empty bucket" {
     var map = try testMap(3);
     defer map.deinit();
 
-    const ret = try map.add(1);
+    const ret = try map.add(1, "foo");
 
     try testing.expect(ret);
     try testing.expectEqualSlices(
         TestMap.Bucket, 
         &[_]TestMap.Bucket { 
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
+            TestMap.Bucket { .full = .{ .key = 1, .value = "foo", .hash_value = 1 } },
             TestMap.Bucket.empty,
         },
         map.buckets
@@ -214,17 +216,17 @@ test "add(value) inserts into bucket when value computes to probed bucket" {
     var map = try testMap(3);
     defer map.deinit();
 
-    _ = try map.add(1);
+    _ = try map.add(1, "bar");
 
-    const ret = try map.add(4);
+    const ret = try map.add(4, "baz");
 
     try testing.expect(ret);
     try testing.expectEqualSlices(
         TestMap.Bucket, 
         &[_]TestMap.Bucket { 
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
-            TestMap.Bucket { .full = .{ .value = 4, .hash_value = 4 } },
+            TestMap.Bucket { .full = .{ .key = 1, .value = "bar", .hash_value = 1 } },
+            TestMap.Bucket { .full = .{ .key = 4, .value = "baz", .hash_value = 4 } },
         },
         map.buckets
     );
@@ -235,21 +237,21 @@ test "add(value) inserts into deleted bucket when value computes to probed bucke
     var map = try testMap(5);
     defer map.deinit();
 
-    _ = try map.add(1);
-    _ = try map.add(2);
-    _ = try map.add(3);
+    _ = try map.add(1, "a");
+    _ = try map.add(2, "b");
+    _ = try map.add(3, "c");
     _ = map.remove(2);
 
-    const ret = try map.add(7);
+    const ret = try map.add(7, "d");
 
     try testing.expect(ret);
     try testing.expectEqualSlices(
         TestMap.Bucket, 
         &[_]TestMap.Bucket { 
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
-            TestMap.Bucket { .full = .{ .value = 7, .hash_value = 7 } },
-            TestMap.Bucket { .full = .{ .value = 3, .hash_value = 3 } },
+            TestMap.Bucket { .full = .{ .key = 1, .value = "a", .hash_value = 1 } },
+            TestMap.Bucket { .full = .{ .key = 7, .value = "d", .hash_value = 7 } },
+            TestMap.Bucket { .full = .{ .key = 3, .value = "c", .hash_value = 3 } },
             TestMap.Bucket.empty,
         },
         map.buckets
@@ -261,15 +263,15 @@ test "add(value) does not insert when value is present at head bucket" {
     var map = try testMap(3);
     defer map.deinit();
 
-    _ = try map.add(1);
-    const ret = try map.add(1);
+    _ = try map.add(1, "a");
+    const ret = try map.add(1, "b");
 
     try testing.expect(!ret);
     try testing.expectEqualSlices(
         TestMap.Bucket, 
         &[_]TestMap.Bucket { 
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
+            TestMap.Bucket { .full = .{ .key = 1, .value = "a", .hash_value = 1 } },
             TestMap.Bucket.empty,
         },
         map.buckets
@@ -281,17 +283,17 @@ test "add(value) does not insert when value is present at probed bucket" {
     var map = try testMap(5);
     defer map.deinit();
 
-    _ = try map.add(6);
-    _ = try map.add(1);
-    const ret = try map.add(1);
+    _ = try map.add(6, "six");
+    _ = try map.add(1, "one");
+    const ret = try map.add(1, "nope");
 
     try testing.expect(!ret);
     try testing.expectEqualSlices(
         TestMap.Bucket, 
         &[_]TestMap.Bucket { 
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 6, .hash_value = 6 } },
-            TestMap.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
+            TestMap.Bucket { .full = .{ .key = 6, .value = "six", .hash_value = 6 } },
+            TestMap.Bucket { .full = .{ .key = 1, .value = "one", .hash_value = 1 } },
             TestMap.Bucket.empty,
             TestMap.Bucket.empty,
         },
@@ -304,21 +306,21 @@ test "add(value) does not insert when value is present at probed bucket across d
     var map = try testMap(5);
     defer map.deinit();
 
-    _ = try map.add(6);
-    _ = try map.add(1);
-    _ = try map.add(3);
+    _ = try map.add(6, "6");
+    _ = try map.add(1, "1");
+    _ = try map.add(3, "3");
     _ = map.remove(1);
 
-    const ret = try map.add(3);
+    const ret = try map.add(3, "3 again");
 
     try testing.expect(!ret);
     try testing.expectEqualSlices(
         TestMap.Bucket, 
         &[_]TestMap.Bucket { 
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 6, .hash_value = 6 } },
+            TestMap.Bucket { .full = .{ .key = 6, .value = "6", .hash_value = 6 } },
             TestMap.Bucket.deleted,
-            TestMap.Bucket { .full = .{ .value = 3, .hash_value = 3 } },
+            TestMap.Bucket { .full = .{ .key = 3, .value = "3", .hash_value = 3 } },
             TestMap.Bucket.empty,
         },
         map.buckets
@@ -330,24 +332,24 @@ test "add(value) rehashes when the load factor is crossed" {
     var map = try testMap(5);
     defer map.deinit();
 
-    _ = try map.add(2); // 2 => 2
-    _ = try map.add(4); // 4 => 4
-    _ = try map.add(7); // 3 => 7
+    _ = try map.add(2, "2");
+    _ = try map.add(4, "4");
+    _ = try map.add(7, "3 then 7");
 
-    _ = try map.add(8); // 8 => 8
+    _ = try map.add(8, "8");
 
     try testing.expectEqualSlices(
         TestMap.Bucket,
         &[_]TestMap.Bucket {
             TestMap.Bucket.empty,
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 2, .hash_value = 2 } },
+            TestMap.Bucket { .full = .{ .key = 2, .value = "2", .hash_value = 2 } },
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 4, .hash_value = 4 } },
+            TestMap.Bucket { .full = .{ .key = 4, .value = "4", .hash_value = 4 } },
             TestMap.Bucket.empty,
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 7, .hash_value = 7 } },
-            TestMap.Bucket { .full = .{ .value = 8, .hash_value = 8 } },
+            TestMap.Bucket { .full = .{ .key = 7, .value = "3 then 7", .hash_value = 7 } },
+            TestMap.Bucket { .full = .{ .key = 8, .value = "8", .hash_value = 8 } },
             TestMap.Bucket.empty,
         },
         map.buckets
@@ -359,10 +361,11 @@ test "remove(value) marks the bucket as deleted when value present at head bucke
     var map = try testMap(5);
     defer map.deinit();
 
-    _ = try map.add(1);
+    _ = try map.add(1, "a");
     const ret = map.remove(1);
 
-    try testing.expectEqual(1, ret);
+    try testing.expect(ret != null);
+    try testing.expectEqualSlices(u8, "a", ret.?);
     try testing.expectEqualSlices(
         TestMap.Bucket,
         &[_]TestMap.Bucket { 
@@ -381,19 +384,19 @@ test "remove(value) marks the bucket as deleted when value present at probed buc
     var map = try testMap(5);
     defer map.deinit();
 
-    _ = try map.add(11);
-    _ = try map.add(6);
-    _ = try map.add(1);
+    _ = try map.add(11, "11");
+    _ = try map.add(6, "6");
+    _ = try map.add(1, "1");
 
     const ret = map.remove(1);
 
-    try testing.expectEqual(1, ret);
+    try testing.expectEqualSlices(u8, "1", ret.?);
     try testing.expectEqualSlices(
         TestMap.Bucket,
         &[_]TestMap.Bucket { 
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 11, .hash_value = 11 } },
-            TestMap.Bucket { .full = .{ .value = 6, .hash_value = 6 } },
+            TestMap.Bucket { .full = .{ .key = 11, .value = "11", .hash_value = 11 } },
+            TestMap.Bucket { .full = .{ .key = 6, .value = "6", .hash_value = 6 } },
             TestMap.Bucket.deleted,
             TestMap.Bucket.empty,
         },
@@ -406,9 +409,9 @@ test "remove(value) returns null when value not present" {
     var map = try testMap(5);
     defer map.deinit();
 
-    _ = try map.add(1);
-    _ = try map.add(2);
-    _ = try map.add(3);
+    _ = try map.add(1, "1");
+    _ = try map.add(2, "2");
+    _ = try map.add(3, "3");
 
     const ret = map.remove(4);
 
@@ -417,9 +420,9 @@ test "remove(value) returns null when value not present" {
         TestMap.Bucket,
         &[_]TestMap.Bucket { 
             TestMap.Bucket.empty,
-            TestMap.Bucket { .full = .{ .value = 1, .hash_value = 1 } },
-            TestMap.Bucket { .full = .{ .value = 2, .hash_value = 2 } },
-            TestMap.Bucket { .full = .{ .value = 3, .hash_value = 3 } },
+            TestMap.Bucket { .full = .{ .key = 1, .value = "1", .hash_value = 1 } },
+            TestMap.Bucket { .full = .{ .key = 2, .value = "2", .hash_value = 2 } },
+            TestMap.Bucket { .full = .{ .key = 3, .value = "3", .hash_value = 3 } },
             TestMap.Bucket.empty,
         },
         map.buckets
@@ -431,9 +434,9 @@ test "has(value) returns true when value is present" {
     var map = try testMap(5);
     defer map.deinit();
 
-    _ = try map.add(1);
-    _ = try map.add(2);
-    _ = try map.add(3);
+    _ = try map.add(1, "1");
+    _ = try map.add(2, "2");
+    _ = try map.add(3, "3");
 
     try testing.expect(map.has(2));
 }
@@ -442,9 +445,9 @@ test "has(value) returns false when value is not present" {
     var map = try testMap(5);
     defer map.deinit();
 
-    _ = try map.add(1);
-    _ = try map.add(2);
-    _ = try map.add(3);
+    _ = try map.add(1, "1");
+    _ = try map.add(2, "2");
+    _ = try map.add(3, "3");
 
     try testing.expect(!map.has(5));
 }
